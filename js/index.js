5e6c7492657dc7d4f809508eb7665face257fc72
di-debug <73755418+di-debug@users.noreply.github.com>
2021-12-21 02:01:33 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function divtwo() {
+    document.getElementById('box-two').style.backgroundColor = 'rgba(139, 139, 155, 0.781)';
+    document.body.style.backgroundColor = 'pink';
+}
+
+function changetext() {
+    document.getElementById('para').innerHTML = ':O.... you changed all the text here ';
+}
+
+function divthree() {
+    document.getElementById('box-three').style.backgroundColor = 'rgba(139, 139, 155, 0.781)';
+    document.body.style.backgroundColor = '#2C2C2C';
+}
